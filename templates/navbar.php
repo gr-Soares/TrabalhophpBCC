ca63101b0f326817263f994ef686071d3dbfd32e
gr-Soares <gustavo.rg.soares@gmail.com>
2023-06-21 21:20:23 -0300

    estrutura inicial

---
+++ templates/navbar.php
@@ -0,0 +1,26 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Estacionamento</a>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Entrada/Saida</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Carros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Clientes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Historico</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
